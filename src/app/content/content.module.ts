9bd056530444ae15d775e9fa7c55a5ce0fe5e434
stopgul <stopgul@gmail.com>
2021-02-21 06:31:51 -0500

    a

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticlesComponent } from './articles/articles.component';
+import { ArticleDetailsComponent } from './articles/article-details/article-details.component';
+import { ArticleItemComponent } from './articles/article-item/article-item.component';
 
 
 
 @NgModule({
-  declarations: [ArticlesComponent],
+  declarations: [ArticlesComponent, ArticleDetailsComponent, ArticleItemComponent],
   imports: [
     CommonModule
   ]
